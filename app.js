ec4eff12584156fe260600dbf2e73510824e2a46
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-07-02 20:31:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// let login = () => {
+//     var a = document.getElementById('email').value
+//     var b = document.getElementById('pass').value
+//     if(a == "admin" && b == "admin"){
+//         // location.href = "quiz.html"
+//         // window.location.assign('./quiz.html')
+//     }
+// }
+
+// let login = () => {
+//     var a = document.getElementById('email').value
+//     var b = document.getElementById('pass').value
+//     if (a == "admin" && b == "admin") {
+//         // location.href = "quiz.html"
+//         // window.location.assign('./quiz.html')
+//         window.location.replace('quiz.html')
+//     }
+// }
+
+// let go = () => {
+//     window.location.replace('index.html')
+// }
+
+// let login = () => {
+//     // window.open().document.write('<h1>quiz</h1>')
+//     // window.open('./quiz.html')
+//     window.open('https://www.facebook.com',"",'width=800,height=200,top=300,left=400')
+// }
+
+console.log(firebase.database)
+
+("/")
